a9fc7a8d3c092d7491a9066247f877ecb9c8469a
seriousboy1904-hub <serious.boy.19.04@gmail.com>
2026-02-02 21:11:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,23 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from config import CLIENT_TOKEN, DRIVER_TOKEN
-from client_handlers import router as client_router
-from driver_handlers import router as driver_router
+from client_handlers import client_router # Nomiga e'tibor bering
+from driver_handlers import driver_router # Nomiga e'tibor bering
 import database as db
 
 async def main():
     logging.basicConfig(level=logging.INFO)
-    
-    # Ma'lumotlar bazasini yaratish
     db.init_db()
 
-    # Botlarni yaratish
-    client_bot = Bot(token=CLIENT_TOKEN)
-    driver_bot = Bot(token=DRIVER_TOKEN)
+    bot_client = Bot(token=CLIENT_TOKEN)
+    bot_driver = Bot(token=DRIVER_TOKEN)
 
     dp = Dispatcher()
-
-    # Routerlarni ulash
     dp.include_router(client_router)
     dp.include_router(driver_router)
 
-    # Ikkala botni bir vaqtda ishga tushirish
-    await dp.start_polling(client_bot, driver_bot)
+    logging.info("Botlar ishga tushdi...")
+    await dp.start_polling(bot_client, bot_driver)
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        logging.info("Bot to'xtatildi")
+    asyncio.run(main())
