e7a1099a776c712732e3707f59ef79b73bc3981b
seriousboy1904-hub <serious.boy.19.04@gmail.com>
2026-02-02 22:41:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
-from dotenv import load_dotenv
 import json
-with open("locations.json", "r", encoding="utf-8") as f:
-    STATIONS = json.load(f)
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 load_dotenv()
+
 CLIENT_TOKEN = os.getenv("CLIENT_TOKEN")
 DRIVER_TOKEN = os.getenv("DRIVER_TOKEN")
+
+with open(os.path.join(BASE_DIR, "locations.json"), "r", encoding="utf-8") as f:
+    STATIONS = json.load(f)
