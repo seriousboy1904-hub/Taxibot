8570d1c94fed428ba5d7e9dcfc3e80b5882cb349
seriousboy1904-hub <serious.boy.19.04@gmail.com>
2026-02-02 20:06:46 +0500

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class Database:
     def __init__(self, db_file):
-        self.connection = sqlite3.connect(db_file)
+        self.connection = sqlite3.connect(db_file, check_same_thread=False)
         self.cursor = self.connection.cursor()
         self.create_tables()
 
