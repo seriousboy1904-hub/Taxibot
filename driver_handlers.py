9b444e34580945c2d77a8580382ba0d662b1a8f0
seriousboy1904-hub <serious.boy.19.04@gmail.com>
2026-02-02 21:10:40 +0500

    Update driver_handlers.py

--- driver_handlers.py
+++ driver_handlers.py
@@ -5,7 +5,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 import database as db
 
-router = Router()
+router = driver_router
 
 # Holatlarni aniqlaymiz
 class DriverReg(StatesGroup):
