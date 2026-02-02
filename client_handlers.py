4bf0a6e2fdeaddf4912c00d11f7f9bdae3a48806
seriousboy1904-hub <serious.boy.19.04@gmail.com>
2026-02-02 22:54:05 +0500

    Update client_handlers.py

--- client_handlers.py
+++ client_handlers.py
@@ -1,39 +1,41 @@
-from aiogram import Router, F, types, Bot
+from aiogram import Router, F, types
+from aiogram import Bot
 from database import Database
 from helpers import find_nearest_station
 from config import DRIVER_TOKEN, STATIONS
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 client_router = Router()
 db = Database("taxi.db")
-driver_bot_sender = Bot(token=DRIVER_TOKEN)
+
+driver_bot = Bot(token=DRIVER_TOKEN)
 
 @client_router.message(F.location)
 async def handle_client_order(message: types.Message):
     c_lat = message.location.latitude
     c_lon = message.location.longitude
-    
-    station_name, dist = find_nearest_station(c_lat, c_lon, STATIONS)
-    
-    # Navbatdagi bo'sh haydovchini olish
-    next_driver = db.get_first_driver_in_queue(station_name)
-    
-    if next_driver:
-        # Haydovchi uchun tugmalar
-        kb = InlineKeyboardMarkup(inline_keyboard=[
-            [
-                InlineKeyboardButton(text="✅ Qabul qilish", callback_data=f"accept_{message.from_user.id}"),
-                InlineKeyboardButton(text="❌ Skip (O'tkazish)", callback_data=f"reject_{message.from_user.id}")
-            ]
-        ])
-        
-        # Haydovchiga xabar
-        await driver_bot_sender.send_message(
-            next_driver[0], # user_id
-            f"🚕 **Yangi buyurtma!**\n\n📍 Bekat: {station_name}\n👤 Mijoz: {message.from_user.full_name}\n\nQabul qilasizmi?",
-            parse_mode="Markdown",
-            reply_markup=kb
+
+    station_name, distance = find_nearest_station(c_lat, c_lon, STATIONS)
+    driver = db.get_first_driver_in_queue(station_name)
+
+    if driver:
+        await driver_bot.send_message(
+            driver["user_id"],
+            (
+                "🚕 YANGI BUYURTMA\n"
+                f"📍 Bekat: {station_name}\n"
+                f"📏 Masofa: {round(distance, 2)}\n"
+                f"👤 Mijoz: {message.from_user.full_name}\n"
+                f"📌 Lokatsiya: {c_lat}, {c_lon}"
+            )
         )
-        await message.answer(f"📍 Sizga eng yaqin bekat: **{station_name}**\n⏳ So'rov haydovchiga yuborildi, javobni kuting...", parse_mode="Markdown")
+
+        await message.answer(
+            f"✅ Buyurtma qabul qilindi\n"
+            f"📍 Bekat: {station_name}\n"
+            "🚗 Haydovchi yo‘lda"
+        )
     else:
-        await message.answer(f"📍 Sizga eng yaqin bekat: {station_name}\n😔 Afsuski, hozircha bu bekatda bo'sh haydovchi yo'q.")
+        await message.answer(
+            "⛔ Hozircha bo‘sh haydovchi yo‘q\n"
+            "⏳ Iltimos kuting"
+        )
